47e326453cbc700d2b49836b7ce97c07658233db
phithithuhuong <thu872315@gmail.com>
2022-10-10 17:15:28 +0700

    md1Bai7-2

---
+++ index.js
@@ -0,0 +1,4 @@
+let celsius = parseInt(prompt("nhập độ 0C"))
+let fahrenheit =((celsius / 5) * 9 + 32)
+// alert("nhập độ f: " + fahrenheit) hiển thị trong màn hình
+alert("nhập độ f: " + fahrenheit)
